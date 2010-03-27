eb5afc3835f8ff0642b85ca55c1ffcfc47d9be92
Irene Knapp <irene.knapp@icloud.com>
2010-03-27 16:19:52 +0000

    Substantial implementation of the C export metacode.

--- Language/SQL/SQLite/C.hs
+++ Language/SQL/SQLite/C.hs
@@ -1,7 +1,6 @@
-{-# LANGUAGE ForeignFunctionInterface, TemplateHaskell #-}
+{-# LANGUAGE ForeignFunctionInterface, TemplateHaskell, QuasiQuotes #-}
 module Language.SQL.SQLite.C where
 
--- For sanity's sake, keep these imports in synch with the ones in CTH.hs.
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.UTF8 as BS hiding (length)
 import Data.Word
@@ -9,6 +8,8 @@ import Foreign.C
 import Foreign.Ptr
 import Foreign.StablePtr
 import Foreign.Storable
+import Language.Haskell.Semiquotation
+
 import Language.SQL.SQLite
 import Language.SQL.SQLite.CTH
 
@@ -18,10 +19,11 @@ free pointer = do
   let stablePointer = castPtrToStablePtr pointer
   freeStablePtr stablePointer
 
-foreign export ccall "hsql_show" showC :: Ptr a -> Ptr CSize -> Ptr CString -> IO ()
-showC :: Ptr a -> Ptr CSize -> Ptr CString -> IO ()
+foreign export ccall "hsql_show" showC :: Ptr () -> Ptr CSize -> Ptr CString -> IO ()
+showC :: Ptr () -> Ptr CSize -> Ptr CString -> IO ()
 showC objectPointer sizePointer bufferPointer = do
-  let bytestring = BS.fromString $ show $ castPtr objectPointer
+  object <- deRefStablePtr $ castPtrToStablePtr objectPointer :: IO [Token]
+  let bytestring = BS.fromString $ show object
   if sizePointer /= nullPtr
     then poke sizePointer $ fromIntegral $ BS.length bytestring + 1
     else return ()   
@@ -34,6 +36,42 @@ showC objectPointer sizePointer bufferPointer = do
       pokeElemOff (castPtr bufferPointer) (BS.length bytestring) (0 :: Word8)
     else return ()
 
-exportShowTokensForType 'Type
+exportType
+  ''Type
+  [("type", 'Type,
+    [("identifier", [t| UnqualifiedIdentifier |]),
+     ("sizes", [t| MaybeTypeSize |])])]
+exportShowTokensForType ''Type
+{-
 
--- foreign export ccall "hsql_Type" typeC :: Ptr () -> Ptr () -> 
+-- It needs to expand several to definitions:
+
+-- A function for each type constructor (in this case we are fortunate enough to only
+-- have one) that constructs the type.
+foreign export ccall "hsql_new_type"
+  newType :: Ptr () -> Ptr () -> IO (Ptr ())
+newType :: Ptr () -> Ptr () -> IO (Ptr ())
+newType a stuff = do
+  a' <- getGeneric (unused :: UnqualifiedIdentifier) a
+  stuff' <- getGeneric (unused :: Maybe ... the long type above) stuff
+  newStablePointer (Type a' stuff') >>= return . castStablePtrToPtr
+
+-- Now one accessor function for each parameter of that type constructor.
+foreign export ccall "hsql_get_type_identifier"
+  getTypeIdentifier :: Ptr () -> IO (Ptr ())
+getTypeIdentifier :: Ptr () -> IO (Ptr ())
+getTypeIdentifier type' = do
+  (Type identifier _) <- getGeneric (unused :: Type) type'
+  resultStablePointer <- newStablePointer $ identifier
+  return $ castStablePtrToPtr resultStablePointer
+
+
+-- Another accessor.
+foreign export ccall "hsql_get_type_stuff"
+  getTypeStuff :: Ptr () -> IO (Ptr ())
+getTypeStuff :: Ptr () -> IO (Ptr ())
+getTypeStuff type' = do
+  (Type _ stuff) <- getGeneric (unused :: Type) type'
+  resultStablePointer <- newStablePointer $ stuff
+  return $ castStablePtrToPtr resultStablePointer
+-}
--- Language/SQL/SQLite/CTH.hs
+++ Language/SQL/SQLite/CTH.hs
@@ -1,62 +1,18 @@
-{-# LANGUAGE ForeignFunctionInterface, TemplateHaskell #-}
-module Language.SQL.SQLite.CTH where
+{-# LANGUAGE ForeignFunctionInterface, TemplateHaskell, Rank2Types #-}
+module Language.SQL.SQLite.CTH (
+                                exportType,
+                                exportShowTokensForType,
+                                showTokensGeneric,
+                                getGeneric
+                               )
+    where
 
 import Data.Char
 import Data.List
+import Foreign
+import Language.Haskell.Semiquotation
 import Language.Haskell.TH.Syntax
-
-
-exportShowTokensForType :: Name -> Q [Dec]
-exportShowTokensForType typeName = do
-  return $ [ForeignD
-            $ ExportF CCall
-                      ("hsql_show_tokens_" ++ (haskellStyleToCStyle $ nameBase typeName))
-                      (mkName $ "showTokens" ++ nameBase typeName)
-                      (AppT (AppT ArrowT
-                                  (AppT (ConT $ mkName "Ptr")
-                                        (ConT $ mkName "()")))
-                            (AppT (ConT $ mkName "IO")
-                                  (AppT (ConT $ mkName "Ptr")
-                                        (ConT $ mkName "()")))),
-            FunD (mkName $ "showTokens" ++ nameBase typeName)
-                 [Clause [VarP $ mkName "objectPointer"]
-                         (NormalB
-                          $ DoE [LetS [ValD (VarP $ mkName "objectStablePointer")
-                                            (NormalB
-                                             $ AppE (VarE $ mkName "castPtrToStablePtr")
-                                                    (VarE $ mkName "objectPointer"))
-                                            []],
-                                 BindS (VarP $ mkName "object")
-                                       (SigE (AppE (VarE $ mkName "deRefStablePtr")
-                                                   (VarE $ mkName "objectStablePointer"))
-                                             (AppT (ConT $ mkName "IO")
-                                                   (ConT $ mkName $ nameBase typeName))),
-                                 LetS [ValD (VarP $ mkName "tokens")
-                                            (NormalB
-                                             $ AppE (VarE $ mkName "showTokens")
-                                                    (VarE $ mkName "object"))
-                                            []],
-                                 BindS (VarP $ mkName "tokensStablePointer")
-                                       (AppE (VarE $ mkName "newStablePtr")
-                                             (VarE $ mkName "tokens")),
-                                 LetS [ValD (VarP $ mkName "tokensPointer")
-                                            (NormalB
-                                             $ AppE (VarE $ mkName "castStablePtrToPtr")
-                                                    (VarE $ mkName "tokensStablePointer"))
-                                            []],
-                                 NoBindS (AppE (VarE 'return)
-                                               (VarE $ mkName "tokensPointer"))])
-                         []]]
-{-
-foreign export ccall "hsql_show_tokens_..." showTokens... :: Ptr () -> IO (Ptr ())
-showTokens... objectPointer = do
-  let objectStablePointer = castPtrToStablePtr objectPointer
-  object <- deRefStablePtr objectStablePointer :: IO ...
-  let tokens = showTokens object
-  tokensStablePointer <- newStablePtr tokens
-  let tokensPointer = castStablePtrToPtr tokensStablePointer
-  return tokensPointer
--}
+import qualified Language.SQL.SQLite as SQL
 
 
 haskellStyleToCStyle :: String -> String
@@ -65,8 +21,6 @@ haskellStyleToCStyle haskellStyle =
         maybeNextWordStart string = findIndex isUpper string
         downcaseWord :: String -> String
         downcaseWord word = map toLower word
-        downcaseFirstCharacter :: String -> String
-        downcaseFirstCharacter string = (toLower $ head string) : tail string
         splitIntoWords :: String -> [String]
         splitIntoWords string = case maybeNextWordStart string of
                                   Nothing -> [string]
@@ -77,3 +31,177 @@ haskellStyleToCStyle haskellStyle =
         words = map downcaseWord $ splitIntoWords $ downcaseFirstCharacter haskellStyle
         result = intercalate "_" words
     in result
+
+
+cStyleToHaskellStyle :: String -> String
+cStyleToHaskellStyle cStyle = cStyle
+
+
+downcaseFirstCharacter :: String -> String
+downcaseFirstCharacter string = (toLower $ head string) : tail string
+
+
+upcaseFirstCharacter :: String -> String
+upcaseFirstCharacter string = (toUpper $ head string) : tail string
+
+
+declarationForForeignExportWithAllPointers :: String -> Name -> Int -> Dec
+declarationForForeignExportWithAllPointers cName haskellName arity
+    = let typeForArity 0 = AppT (ConT $ mkName "IO")
+                                (AppT (ConT $ mkName "Ptr")
+                                      (ConT $ mkName "()"))
+          typeForArity n = AppT (AppT ArrowT
+                                      (AppT (ConT $ mkName "Ptr")
+                                            (ConT $ mkName "()")))
+                                $ typeForArity $ n - 1
+      in ForeignD $ ExportF CCall cName haskellName $ typeForArity arity
+{-
+foreign export ccall "cName" haskellName :: Ptr () -> ... -> IO (Ptr ())
+Where Ptr () is repeated arity times.
+-}
+
+
+simpleLetStatement :: String -> Exp -> Stmt
+simpleLetStatement boundVariable body
+    = LetS [ValD (VarP $ mkName boundVariable)
+                 (NormalB body)
+                 []]
+{-
+let boundVariable = body
+In do-notation.
+-}
+
+
+simpleBindStatement :: String -> Exp -> Stmt
+simpleBindStatement boundVariable body
+    = BindS (VarP $ mkName boundVariable)
+            body
+{-
+boundVariable <- body
+In do-notation.
+-}
+
+
+constructionExpression :: Name -> [Exp] -> Exp
+constructionExpression name parameters =
+    let applyToParameters [] = ConE name
+        applyToParameters (parameter:rest) = AppE (applyToParameters rest)
+                                                  parameter
+    in applyToParameters $ reverse parameters
+{-
+Name a1 ...
+-}
+
+
+exportConstructor :: Name -> String -> Name -> [(Name, Type)] -> Q [Dec]
+exportConstructor typeName constructorCName constructorHaskellName parameters = do
+  let arity = length parameters
+      parameterNames = map fst parameters
+      haskellName = (mkName $ "new" ++ (upcaseFirstCharacter
+                                        $ nameBase constructorHaskellName))
+  return $ [declarationForForeignExportWithAllPointers
+              ("hsql_new_" ++ constructorCName)
+              haskellName
+              arity,
+            FunD haskellName
+                 [Clause
+                  (map VarP parameterNames)
+                  (NormalB
+                   $ DoE
+                   $ concat
+                     [(map (\(parameterName, parameterType) ->
+                              simpleBindStatement
+                                (nameBase parameterName)
+                                (AppE (AppE (VarE $ mkName "getGeneric")
+                                            (SigE (VarE $ mkName "undefined")
+                                                  parameterType))
+                                      (VarE parameterName)))
+                           parameters),
+                      [NoBindS
+                       $ InfixE (Just
+                                 (AppE (VarE $ mkName "newStablePtr")
+                                       (constructionExpression
+                                          constructorHaskellName
+                                          $ map VarE parameterNames)))
+                                (VarE $ mkName ">>=")
+                                (Just
+                                 (InfixE (Just (VarE $ mkName "return"))
+                                         (VarE $ mkName ".")
+                                         (Just
+                                          (VarE $ mkName "castStablePtrToPtr"))))]])
+                 []]]
+{-
+foreign export ccall "hsql_new_type"
+  newType :: Ptr () -> Ptr () -> IO (Ptr ())
+newType :: Ptr () -> Ptr () -> IO (Ptr ())
+newType a1 ... = do
+  a1 <- getGeneric (unused :: UnqualifiedIdentifier) a1
+  ... -- Repetitions of the above line for each parameter of the data constructor
+  newStablePointer (Type a' stuff') >>= return . castStablePtrToPtr
+-}
+
+
+exportAccessor :: String -> Name -> String -> Type -> Int -> Int -> Q [Dec]
+exportAccessor constructorCName constructorHaskellName
+               parameterCName parameterType parameterIndex
+               arity
+    = return []
+
+
+exportType :: Name -> [(String, Name, [(String, Q Type)])] -> Q [Dec]
+exportType typeName definitions = do
+  mapM (\(constructorCName, constructorHaskellName, parameterDefinitions) -> do
+          let parameterCNames = map fst parameterDefinitions
+              parameterHaskellNames = map (mkName . cStyleToHaskellStyle) parameterCNames
+              arity = length parameterDefinitions
+          parameterTypes <- mapM id $ map snd parameterDefinitions
+          constructorFunctionDefinitions
+              <- exportConstructor typeName constructorCName constructorHaskellName
+                                   $ zip parameterHaskellNames parameterTypes
+          accessorFunctionDefinitions
+              <- mapM (\(parameterIndex, parameterCName, parameterType) -> do
+                         exportAccessor constructorCName
+                                        constructorHaskellName
+                                        parameterCName
+                                        parameterType
+                                        parameterIndex
+                                        arity)
+                      (zip3 [0..] parameterCNames parameterTypes) >>= return . concat
+          return $ concat [constructorFunctionDefinitions,
+                           accessorFunctionDefinitions])
+       definitions >>= return . concat
+{-
+As exportConstructor and exportAccessor, for every given constructor and accessor.
+-}
+
+
+exportShowTokensForType :: Name -> Q [Dec]
+exportShowTokensForType typeName = do
+  return $ [declarationForForeignExportWithAllPointers
+              ("hsql_show_tokens_" ++ (haskellStyleToCStyle $ nameBase typeName))
+              (mkName $ "showTokens" ++ nameBase typeName)
+              1,
+            FunD (mkName $ "showTokens" ++ nameBase typeName)
+                 [Clause [VarP $ mkName "objectPointer"]
+                         (NormalB
+                          $ (AppE (AppE (VarE $ mkName "showTokensGeneric")
+                                        (SigE (VarE $ mkName "undefined")
+                                              (ConT $ typeName)))
+                                  (VarE $ mkName "objectPointer")))
+                         []]]
+{-
+foreign export ccall "hsql_show_tokens_..." showTokens... :: Ptr () -> IO (Ptr ())
+showTokens... objectPointer = showTokensGeneric (undefined :: ...) objectPointer
+-}
+
+
+showTokensGeneric :: (SQL.ShowTokens a) => a -> Ptr () -> IO (Ptr ())
+showTokensGeneric unused objectPointer = do
+  object <- asTypeOf (return unused) $ deRefStablePtr $ castPtrToStablePtr objectPointer
+  tokensStablePointer <- newStablePtr $ SQL.showTokens object
+  return $ castStablePtrToPtr tokensStablePointer
+
+
+getGeneric :: forall a . a -> Ptr () -> IO a
+getGeneric unused objectPointer = do
+  asTypeOf (return unused) $ deRefStablePtr $ castPtrToStablePtr objectPointer
